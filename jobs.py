cf03afa9f4f868b834046856b99cad6e3ec28cd4
ME_Kuroko007 <98237827+MEKuroko007@users.noreply.github.com>
2022-09-22 14:52:03 +0100

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver import Chrome, ChromeOptions
-import os.path
+
 
 #//////////////////////////////////////////////////
 #this script is for serach job in 'emploi public'

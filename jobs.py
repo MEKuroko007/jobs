fe9802f1421424b819cc21a7e0009b59c2fba3d4
ME_Kuroko007 <98237827+MEKuroko007@users.noreply.github.com>
2022-09-22 15:27:34 +0100

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -1,3 +1,4 @@
+from msilib.schema import Directory
 from ssl import Options
 from time import sleep
 from selenium import webdriver
@@ -5,17 +6,20 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver import Chrome, ChromeOptions
+import os
 
 
 #//////////////////////////////////////////////////
-#this script is for serach job in 'emploi public'
-#author ESSALHI MUSTAPHA {kUROKO007}
+#This is  a job search script on the "emploi public"
+#author ESSALHI {kUROKO007}
 #//////////////////////////////////////////////////
-
+directory=f"C:\\Users\\{os.getlogin()}\\Desktop\\Jobs"
+if not os.path.exists(directory):
+    os.makedirs(directory)
 #########################################33
 options = webdriver.ChromeOptions()
 options.add_experimental_option("prefs", {
-  "download.default_directory": "D:\\Backup",#you must create this directory or change it
+  "download.default_directory": directory,
   "download.prompt_for_download": False,
   "download.directory_upgrade": True,
   "safebrowsing.enabled": True,
@@ -28,9 +32,8 @@ links=[]
 driver.get("https://www.emploi-public.ma/FR/index.asp?p=1")
 el=driver.find_elements(By.CSS_SELECTOR,"td a")
 for e in el:
-    #if "Technicien" in e.text and "9" in e.text:
-    if "Echelle 9"in e.text : 
-        links.append(e.get_attribute("href"))   
+    if "Echelle 9" in e.text : # if you want to search for another level,only change number 9 EX:'TS:9,lp:10.....'
+      links.append(e.get_attribute("href")) 
 for link in links:
     driver.get(link)
     driver.find_element(By.XPATH,"//a[@class='btn btn-block btn-social btn-down']").click()

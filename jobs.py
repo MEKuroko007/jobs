318763a20ec531fd3d9df2afc9fe5d2bf68dd7e8
ME_Kuroko007 <98237827+MEKuroko007@users.noreply.github.com>
2022-09-23 16:51:43 +0100

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -7,16 +7,16 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver import Chrome, ChromeOptions
 import os
-
-
+import hashlib
+from pathlib import Path
 #//////////////////////////////////////////////////
 #This is  a job search script on the "emploi public"
-#author ESSALHI {kUROKO007}
+################ ESSALHI {kUROKO007} ##############
 #//////////////////////////////////////////////////
 directory=f"C:\\Users\\{os.getlogin()}\\Desktop\\Jobs"
 if not os.path.exists(directory):
     os.makedirs(directory)
-#########################################33
+######################################################
 options = webdriver.ChromeOptions()
 options.add_experimental_option("prefs", {
   "download.default_directory": directory,
@@ -32,10 +32,25 @@ links=[]
 driver.get("https://www.emploi-public.ma/FR/index.asp?p=1")
 el=driver.find_elements(By.CSS_SELECTOR,"td a")
 for e in el:
-    if "Echelle 9" in e.text : # if you want to search for another level,only change number 9 EX:'TS:9,lp:10.....'
+    if "Echelle 11" in e.text : # if you want to search for another level,only change number 9 EX:'TS:9,lp:10.....'
       links.append(e.get_attribute("href")) 
 for link in links:
     driver.get(link)
     driver.find_element(By.XPATH,"//a[@class='btn btn-block btn-social btn-down']").click()
-sleep(50)
+
+##############################################################
+#################Remove duplicate Files#######################
+##############################################################
+file_path =f"C:\\Users\\{os.getlogin()}\\Desktop\\jobs"
+list_files = os.walk(file_path)
+unique_files = dict()
+for root, folders, files in list_files:
+    for file in files:
+        file_path = Path(os.path.join(root, file))
+        Hash_file = hashlib.md5(open(file_path, 'rb').read()).hexdigest()
+        if Hash_file not in unique_files:
+            unique_files[Hash_file] = file_path
+        else:
+            os.remove(file_path)
+sleep(100)
 driver.quit()
